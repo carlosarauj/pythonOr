a8b14c5873f5c6d1c355a35d59b8e31c8fcf7548
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-06-05 19:01:09 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,13 @@
+entrada = input('[E]ntrar [S]air: ' )
+senhadig = input("Senha: ")
+
+senha_permitida = '123456'
+
+if (entrada == 'E' or entrada == 'e') and senhadig == senhadig == senha_permitida:
+    print('entrou')
+else:
+    print('Saiu')
+
+
+
+#python py.py
